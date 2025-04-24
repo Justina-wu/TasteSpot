2c9d9b2aa5fca111e0672ffba88a2fb1d270c5e9
Justina <justina_wu@163.com>
2025-04-24 22:19:05 +0800

    login implements

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,41 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.RefreshTokenInterceptor;
+import com.hmdp.utils.loginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * ClassName: MvcConfig
+ * Package: com.hmdp.config
+ * Description:
+ *
+ * @Author JIAYI WU
+ * @Create 2025/4/24 20:59
+ * @Version 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new loginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
+}
