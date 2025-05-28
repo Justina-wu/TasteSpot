01fc5d1b88e840c22d0762f5797d192b1ad4ea17
Justina <justina_wu@163.com>
2025-05-28 10:18:21 +0800

    秒杀优化

--- src/test/java/com/hmdp/GetTokensAndWriteFiles.java
+++ src/test/java/com/hmdp/GetTokensAndWriteFiles.java
@@ -3,40 +3,23 @@ package com.hmdp;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.lang.UUID;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.service.IUserService;
-import com.hmdp.service.impl.ShopServiceImpl;
-import com.hmdp.utils.RedisConstants;
-import com.hmdp.utils.RedisIdWorker;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
-
 import javax.annotation.Resource;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 @SpringBootTest
-class HmDianPingApplicationTests {
-
-    @Resource
-    private ShopServiceImpl shopService;
-
-    @Resource
-    private RedisIdWorker redisIdWorker;
+public class GetTokensAndWriteFiles {
 
     @Resource
     private IUserService userService;
@@ -44,37 +27,10 @@ class HmDianPingApplicationTests {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
-    private ExecutorService es = Executors.newFixedThreadPool(500);
-    @Test
-    void testIdWorker() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(300);
-        Runnable task = () -> {
-            for (int i = 0; i < 100; i++) {
-                long id = redisIdWorker.nextId("order");
-                System.out.println("id = " + id);
-            }
-            latch.countDown();
-        };
-        long begin = System.currentTimeMillis();
-        for (int i = 0; i < 300; i++) {
-            es.submit(task);
-        }
-
-        latch.await();
-        long end = System.currentTimeMillis();
-        System.out.println("time = " +(end - begin));
-    }
-
-    @Test
-    void testSaveShop() throws InterruptedException {
-        shopService.saveShop2Redis(1L, 10L);
-    }
-
-
     @Test
     void getTokensAndWriteFiles() throws IOException {
         List<User> users = userService.lambdaQuery().last("limit 1000").list();
-        java.io.FileWriter fr = null;
+        FileWriter fr = null;
         try {
             fr = new FileWriter("E:\\A-WJY\\CODE\\TasteSpot\\files\\tokens.txt");//文件位置
             for (User user : users) {
@@ -103,5 +59,5 @@ class HmDianPingApplicationTests {
             fr.close();
         }
     }
-
 }
+
