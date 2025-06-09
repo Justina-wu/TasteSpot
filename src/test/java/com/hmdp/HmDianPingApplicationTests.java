02f5cbeee3f3d2fb014c0191e971497231475e8a
Justina <justina_wu@163.com>
2025-06-09 20:49:09 +0800

    修改pom之前

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -5,6 +5,7 @@ import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.lang.UUID;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.Shop;
 import com.hmdp.entity.User;
 import com.hmdp.service.IUserService;
 import com.hmdp.service.impl.ShopServiceImpl;
@@ -12,12 +13,15 @@ import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,9 +29,9 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
-import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -101,6 +105,32 @@ class HmDianPingApplicationTests {
             System.out.println(e.getMessage());
         }finally{
             fr.close();
+        }
+    }
+
+    @Test
+    void loadShopData(){
+        // 1. 查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2. 讲店铺分组，按照typeId
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3. 分批写入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1 获取类型id
+            Long typeId = entry.getKey();
+            // 3.2 获取同类型店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3 写入redis  GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+//                stringRedisTemplate.opsForGeo()
+//                        .add(SHOP_GEO_KEY, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(SHOP_GEO_KEY + typeId, locations);
         }
     }
 
